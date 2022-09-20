8b694ff3555e893fd31cb7bb59706360014563d2
Annderson Rey <anndersonrey@gmail.com>
2022-09-19 21:49:03 -0500

    Make Model Product CRUD

--- backend/Commercial_Manager_API/api/models.py
+++ backend/Commercial_Manager_API/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.CharField(max_length=40)
+    type_product = models.CharField(max_length=30)
--- backend/Commercial_Manager_API/api/views.py
+++ backend/Commercial_Manager_API/api/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from itertools import product
+import json
+from unicodedata import category
 
-# Create your views here.
+from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Product
+
+
+class ProductView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if(id>0):
+            products = list(Product.objects.filter(id=id).values())
+            if len(products) > 0:
+                product = products[0]
+                data = {'message':'Success', 'product': product}
+            else:
+                data = {'message': 'Product not found'}
+            return JsonResponse(data)
+        else:
+            products = list(Product.objects.values())
+            if len(products) > 0:
+                data = {'message':'Success', 'products': products}
+            else:
+                data = {'message': 'Products not found'}
+            return JsonResponse(data)
+
+    def post(self, request):
+        json_data = json.loads(request.body)
+        Product.objects.create(name=json_data['name'], category=json_data['category'], type_product=json_data['type_product'])
+        data = {'message': 'Success'}
+        return JsonResponse(data)
+
+    def put(self, request, id):
+        json_data = json.loads(request.body)
+        products = list(Product.objects.filter(id=id).values())
+        if len(products) > 0:
+            product = Product.objects.get(id=id)
+            product.name = json_data['name']
+            product.category = json_data['category']
+            product.type_product = json_data['type_product']
+            product.save()
+            data = {'message': 'Success'}
+        else:
+            data = {'message': 'Product not found'}
+        return JsonResponse(data)
+    
+    def delete(self, request, id):
+        products = list(Product.objects.filter(id=id).values())
+        if len(products) > 0:
+            Product.objects.filter(id=id).delete()
+            data = {'message': 'Success'}
+        else:
+            data = {'message': 'Product not found'}
+        return JsonResponse(data)
+
